4eb9f85c98103f6f65f78664751f97e2134e55ed
caspar.mayrgundter <caspar.mayrguendter@gmail.com>
2025-12-05 09:46:16 +0100

    add upcoming show in loop

--- src/data/upcomingShows.ts
+++ src/data/upcomingShows.ts
@@ -10,25 +10,9 @@ export interface UpcomingShow {
 export const upcomingShows: UpcomingShow[] = [
   {
     id: '1',
-    date: '2025-02-15',
-    time: '20:00',
-    venue: 'Reigen',
+    date: '2025-12-19',
+    time: '19:00',
+    venue: 'Loop',
     city: 'Wien',
-    ticketLink: 'https://example.com/tickets',
-  },
-  {
-    id: '2',
-    date: '2025-03-22',
-    time: '19:30',
-    venue: 'Stockwerk',
-    city: 'Wien',
-    ticketLink: 'https://example.com/tickets',
-  },
-  {
-    id: '3',
-    date: '2025-04-10',
-    time: '20:00',
-    venue: 'Vorstadttheater',
-    city: 'Wien',
-  },
+  }
 ];
