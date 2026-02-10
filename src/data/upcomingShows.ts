0ac30d1e539ab79b1ea7204317e13bffeffa00d3
caspar.mayrgundter <caspar.mayrguendter@gmail.com>
2026-02-10 21:37:34 +0100

    add G5-gig

--- src/data/upcomingShows.ts
+++ src/data/upcomingShows.ts
@@ -10,9 +10,10 @@ export interface UpcomingShow {
 export const upcomingShows: UpcomingShow[] = [
   {
     id: '1',
-    date: '2025-12-19',
-    time: '19:30',
-    venue: 'Loop',
+    date: '2026-04-24',
+    time: '19:00',
+    venue: 'G5 - Live Music Bar',
     city: 'Wien',
+    ticketLink: 'https://ticket1389.tickethome.at/buchen/250/24.04.2026/1900=&ref=lena',
   }
 ];
