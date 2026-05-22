277b94662d11d68a7dac2a97a30ac413e4cc708b
caspar.mayrgundter <caspar.mayrguendter@gmail.com>
2026-05-22 08:23:05 +0200

    add lucia gig

--- src/data/upcomingShows.ts
+++ src/data/upcomingShows.ts
@@ -10,10 +10,9 @@ export interface UpcomingShow {
 export const upcomingShows: UpcomingShow[] = [
   {
     id: '1',
-    date: '2026-04-24',
-    time: '19:00',
-    venue: 'G5 - Live Music Bar',
+    date: '2026-06-17',
+    time: '20:00',
+    venue: 'Club Lucia',
     city: 'Wien',
-    ticketLink: 'https://ticket1389.tickethome.at/buchen/250/24.04.2026/1900=&ref=lena',
   }
 ];
